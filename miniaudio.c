d0818ec39558d6cc98f02b8f27e6f3b1fa0b28cd
Eli Aloni <aloni.eli@gmail.com>
2026-05-10 09:39:53 +0300

    sokol-dev , add support fro Ogg/Vorbis

--- miniaudio.c
+++ miniaudio.c
@@ -1,2 +1,6 @@
+// Include stb_vorbis before miniaudio.h so that miniaudio's built-in Ogg/Vorbis
+// decoder is enabled (guarded by STB_VORBIS_INCLUDE_STB_VORBIS_H).
+#include "extras/stb_vorbis.c"
+
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
