8a32f003ca7156b65e184b406a9e0fcc17493e4a
Eli Aloni <aloni.eli@gmail.com>
2026-05-10 09:45:32 +0300

    sokol-dev , fix Windows build error

--- miniaudio.c
+++ miniaudio.c
@@ -2,5 +2,15 @@
 // decoder is enabled (guarded by STB_VORBIS_INCLUDE_STB_VORBIS_H).
 #include "extras/stb_vorbis.c"
 
+// stb_vorbis defines L, C, R and PLAYBACK_* macros without cleaning them up.
+// They leak into subsequent headers (especially Windows SDK winnt.h) and cause
+// syntax errors. Undef them here before including miniaudio.h.
+#undef L
+#undef C
+#undef R
+#undef PLAYBACK_MONO
+#undef PLAYBACK_LEFT
+#undef PLAYBACK_RIGHT
+
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
